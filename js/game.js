8297aa91ce5664cf42e8e3567cff5d1a01c4a1c6
Greg Roodt <groodt@gmail.com>
2013-02-25 20:24:53 +0000

    Commit final game at end of play of game jam.

--- js/game.js
+++ js/game.js
@@ -4,12 +4,15 @@
     //
     Crafty.c('Ostrich', {
         init: function() {
-            this.requires('2D, DOM, Color, Keyboard, Collision, Delay');
+            this.requires('2D, DOM, Color, Keyboard, Collision, Delay, SpriteAnimation, run');
             this.color('#f00');
-            this.attr({w: 100, h: 100, x: 400, y: 300});
+            this.attr({w: 550, h: 400, x: 100, y: 100});
             this.baseY = this.y;
             this.baseH = this.h;
             this.collision();
+            //this.animate('duck', 0, 0, 1);
+            this.animate('run', 0, 0, 1);
+            this.animate('run', 5, -1);
             this.bind('EnterFrame', function() {
                 if (this.y < this.baseY) {
                     this.y = this.y + 5;
@@ -32,18 +35,22 @@
             this.y = this.baseY - 200;
         },
         duck: function() {
+            this.stop();
+            //this.animate('duck', 5, -1);
             this.h = this.h - 30;
             this.y = this.y + 30;
             this.delay(function() {
                 this.h = this.baseH;
                 this.y = this.y - 30;
+                //this.stop();
+                this.animate('run', 5, -1);
             }, 1000);
         }
     });
 
     Crafty.c('Obsticle', {
         init: function() {
-            this.requires('2D, DOM, Color, Delay, Collision');
+            this.requires('2D, DOM, Color, Delay, Collision, Persist');
             this.attr({w: 30, h: 30});
             this.collision();
             this.color('#0f0');
@@ -59,14 +66,34 @@
     Crafty.c('Bush', {
         init: function() {
             this.requires('Obsticle');
-            this.attr({x: 1024, y: 350});
+            this.attr({x: 1024, y: 450});
         }
     });
 
     Crafty.c('Branch', {
         init: function() {
             this.requires('Obsticle');
-            this.attr({x: 1024, y: 300});
+            this.attr({x: 1024, y: 50});
+        }
+    });
+
+    Crafty.c('Background', {
+        init: function() {
+            this.bgs = [];
+            for(var i=0; i< 3; i++) {
+                var bg = Crafty.e('2D, DOM, Image, Persist')
+                    .attr({w: Crafty.viewport.width, h: Crafty.viewport.height})
+                    .image('img/savanna_background.jpg')
+                    .bind('EnterFrame', function() {
+                        this.x = this.x - 10;
+                        if (this.x < -883) {
+                            this.x = 883 * 2;
+                        }
+                    });
+                bg.attr({x: i * 883, y: 0});   
+                this.bgs.push(bg); 
+            }
+            
         }
     });
 
@@ -76,6 +103,7 @@
     var Game = function() {
         Crafty.scene('loading', this.loadingScene);
         Crafty.scene('main', this.mainScene);
+        Crafty.scene('gameOver', this.gameOverScene);
     };
     
     Game.prototype.initCrafty = function() {
@@ -97,7 +125,12 @@
         loading.text('loading...');
         
         function onLoaded() {
-            
+            Crafty.sprite(550, 'img/run.png', {
+                run: [0, 0]
+            });
+            Crafty.sprite(550, 'img/duckrun.png', {
+                duck: [0, 0]
+            });
             // jump to the main scene in half a second
             loading.delay(function() {
                 Crafty.scene('main');
@@ -114,7 +147,9 @@
         
         Crafty.load([
             // list of images to load
-            'img/shooter-sprites.png'
+            'img/savanna_background.jpg',
+            'img/run.png',
+            'img/duckrun.png'
         ], 
         onLoaded, onProgress, onError);
         
@@ -124,7 +159,18 @@
     // The main game scene
     //
     Game.prototype.mainScene = function() {
-        Crafty.e('Ostrich');
+
+        var aud = new Audio('snd/Ostrich.wav');
+        aud.loop = true;
+        aud.play();
+        Crafty.e('Background');
+
+        Crafty.e('Ostrich')
+            .bind('Remove', function() {
+                Crafty.e('Delay').delay(function() {
+                    Crafty.scene('gameOver'); 
+                }, 100);
+            });
 
         function createObsticle() {
             Crafty.e('Bush')
@@ -135,6 +181,13 @@
         }
 
         createObsticle();
+    };
+
+    Game.prototype.gameOverScene = function() {
+        var loading = Crafty.e('2D, Canvas, Text, Delay');
+        loading.attr({x: 512, y: 200, w: 100, h: 20});
+        loading.text('Game Over');
+        
     };
         
     
